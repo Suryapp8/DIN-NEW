a22a83d9ebe73e07b6bc14a803e1714dbb959eb0
Suryapp8 <suryapp876@gmail.com>
2023-10-18 11:11:50 +0530

    website is now responsive

--- dumpitnow/src/firebase/firebase.js
+++ dumpitnow/src/firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXITvlaxUKWJlPA7rATVzarIVr2SKGHSM",
-    authDomain: "dump-it-now.firebaseapp.com",
-    projectId: "dump-it-now",
-    storageBucket: "dump-it-now.appspot.com",
-    messagingSenderId: "424145624785",
-    appId: "1:424145624785:web:35f115390d8b61a2ae6ff0",
-    measurementId: "G-RQFXN1XPV4"
+    apiKey: "AIzaSyDD1pzlea_PtW49VJAgYlHdEb3sXn7l1EQ",
+  authDomain: "dump-it-now-c70e5.firebaseapp.com",
+  projectId: "dump-it-now-c70e5",
+  storageBucket: "dump-it-now-c70e5.appspot.com",
+  messagingSenderId: "307814310131",
+  appId: "1:307814310131:web:0a216840c527578259d786",
+  measurementId: "G-BL8PPV4KDR"
 };
 
   
