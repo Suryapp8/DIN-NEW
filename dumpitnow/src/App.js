a41005eb63f81074ad7d8da870ce11f7010df952
Suryapp8 <suryapp876@gmail.com>
2024-03-06 12:29:05 +0530

    added loading screen

--- dumpitnow/src/App.js
+++ dumpitnow/src/App.js
@@ -1,29 +1,48 @@
 import About from "./components/About";
+import React, { useState, useEffect } from "react";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Order from "./components/Order";
 import Ratelist from "./components/Ratelist";
+import Loading from "./components/Loading";
 import "./styles/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Removed `Switch`
 import { Tooltip } from "react-tooltip";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // Adjust loading time as needed
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Tooltip id="my-tooltip" />
-        <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/orderpage" element={<Order />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/ratelist" element={<Ratelist />} />
-        
-        </Routes>
-        <Footer />
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <Navbar />
+            <Tooltip id="my-tooltip" />
+            <Routes>
+              <Route path="/" element={<Home />} /> 
+              <Route path="/orderpage" element={<Order />} />{" "}
+            
+              <Route path="/about" element={<About />} />{" "}
+              
+              <Route path="/ratelist" element={<Ratelist />} />{" "}
+              
+            </Routes>
+            <Footer />
+          </>
+        )}
       </BrowserRouter>
-
     </div>
   );
 }
---
+++ dumpitnow/src/components/Loading.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/loading.css';
+
+const Loading = () => {
+  return (
+    <>
+    <div className='loading-div'>
+    <div className="loader"></div>
+    </div>
+    
+    </>
+    
+    
+  );
+};
+
+export default Loading;
