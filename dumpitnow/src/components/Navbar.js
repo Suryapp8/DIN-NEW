dcbc60eb9734a16e467b575cee0e4e0f059ca3e8
Suryapp8 <suryapp876@gmail.com>
2024-09-24 18:42:09 +0530

    fixed cart button

--- dumpitnow/src/components/Navbar.js
+++ dumpitnow/src/components/Navbar.js
@@ -1,3 +1,79 @@
+// // import React, { useState } from "react";
+// // import { Link } from "react-router-dom";
+// // import logo from "../images/din-logo.png";
+// // import "../styles/navbar.css";
+
+// // function Navbar({ cartCount }) {
+// //   const [menuOpen, setMenuOpen] = useState(false);
+
+// //   const toggleMenu = () => {
+// //     setMenuOpen((prev) => !prev);
+// //   };
+
+// //   const closeMenu = () => {
+// //     setMenuOpen(false);
+// //   };
+
+// //   return (
+// //     <nav>
+// //       <div className="logo-cont">
+// //         <Link to="/">
+// //           <img className="logo-img" src={logo} alt="logo" />
+// //         </Link>
+// //       </div>
+
+// //       <div className={`menu ${menuOpen ? "active" : ""}`}>
+// //         <ul className="nav-links">
+// //           <li className="links">
+// //             <a href="#about" onClick={closeMenu}>
+// //               About
+// //             </a>
+// //           </li>
+// //           <li className="links">
+// //             <Link to="/ratelist" onClick={closeMenu}>
+// //               Scrap List
+// //             </Link>
+// //           </li>
+// //           <li className="links">
+// //             <Link to="/plant" onClick={closeMenu}>
+// //               Plants
+// //             </Link>
+// //           </li>
+
+// //           <li className="links">
+// //             <a href="#supplies" onClick={closeMenu}>
+// //               Supplies
+// //             </a>
+// //           </li>
+// //           <li className="links">
+// //             <a href="#valuate" onClick={closeMenu}>
+// //               How we valuate
+// //             </a>
+// //           </li>
+
+// //           <li className="links">
+// //             <a href="#contact" onClick={closeMenu}>
+// //               Contact
+// //             </a>
+// //           </li>
+// //           <li className="links">
+// //             <Link to="/cart" onClick={closeMenu}>
+// //               🛒 Your Garden ({cartCount})
+// //             </Link>
+// //           </li>
+// //         </ul>
+// //       </div>
+
+// //       {/* Hamburger menu for mobile view */}
+// //       <div className="hamburger" onClick={toggleMenu}>
+// //         ☰
+// //       </div>
+// //     </nav>
+// //   );
+// // }
+
+// // export default Navbar;
+
 // import React, { useState } from "react";
 // import { Link } from "react-router-dom";
 // import logo from "../images/din-logo.png";
@@ -39,24 +115,22 @@
 //               Plants
 //             </Link>
 //           </li>
-
-//           <li className="links">
-//             <a href="#supplies" onClick={closeMenu}>
-//               Supplies
-//             </a>
-//           </li>
 //           <li className="links">
 //             <a href="#valuate" onClick={closeMenu}>
 //               How we valuate
 //             </a>
 //           </li>
-
+//           <li className="links">
+//             <Link to="/supplies" onClick={closeMenu}>
+//               Supplies
+//             </Link>
+//           </li>
 //           <li className="links">
 //             <a href="#contact" onClick={closeMenu}>
 //               Contact
 //             </a>
 //           </li>
-//           <li className="links">
+//           <li className="links cart">
 //             <Link to="/cart" onClick={closeMenu}>
 //               🛒 Your Garden ({cartCount})
 //             </Link>
@@ -66,7 +140,9 @@
 
 //       {/* Hamburger menu for mobile view */}
 //       <div className="hamburger" onClick={toggleMenu}>
-//         ☰
+//         &#9776;
+//         {cartCount > 0 && <span className="cart-count">{cartCount}</span>}{" "}
+//         {/* Cart count on the hamburger */}
 //       </div>
 //     </nav>
 //   );
@@ -75,12 +151,13 @@
 // export default Navbar;
 
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../images/din-logo.png";
 import "../styles/navbar.css";
 
 function Navbar({ cartCount }) {
   const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
@@ -98,52 +175,58 @@ function Navbar({ cartCount }) {
         </Link>
       </div>
 
-      <div className={`menu ${menuOpen ? "active" : ""}`}>
-        <ul className="nav-links">
-          <li className="links">
-            <a href="#about" onClick={closeMenu}>
-              About
-            </a>
-          </li>
-          <li className="links">
-            <Link to="/ratelist" onClick={closeMenu}>
-              Scrap List
-            </Link>
-          </li>
-          <li className="links">
-            <Link to="/plant" onClick={closeMenu}>
-              Plants
-            </Link>
-          </li>
-          <li className="links">
-            <a href="#valuate" onClick={closeMenu}>
-              How we valuate
-            </a>
-          </li>
-          <li className="links">
-            <Link to="/supplies" onClick={closeMenu}>
-              Supplies
-            </Link>
-          </li>
-          <li className="links">
-            <a href="#contact" onClick={closeMenu}>
-              Contact
-            </a>
-          </li>
-          <li className="links cart">
-            <Link to="/cart" onClick={closeMenu}>
-              🛒 Your Garden ({cartCount})
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      {/* Hamburger menu for mobile view */}
-      <div className="hamburger" onClick={toggleMenu}>
-        &#9776;
-        {cartCount > 0 && <span className="cart-count">{cartCount}</span>}{" "}
-        {/* Cart count on the hamburger */}
-      </div>
+      {location.pathname === "/" ? (
+        <>
+          <div className={`menu ${menuOpen ? "active" : ""}`}>
+            <ul className="nav-links">
+              <li className="links">
+                <a href="#about" onClick={closeMenu}>
+                  About
+                </a>
+              </li>
+              <li className="links">
+                <Link to="/ratelist" onClick={closeMenu}>
+                  Scrap List
+                </Link>
+              </li>
+              <li className="links">
+                <Link to="/plant" onClick={closeMenu}>
+                  Plants
+                </Link>
+              </li>
+              <li className="links">
+                <a href="#valuate" onClick={closeMenu}>
+                  How we valuate
+                </a>
+              </li>
+              <li className="links">
+                <Link to="/supplies" onClick={closeMenu}>
+                  Supplies
+                </Link>
+              </li>
+              <li className="links">
+                <a href="#contact" onClick={closeMenu}>
+                  Contact
+                </a>
+              </li>
+              <li className="links cart">
+                <Link to="/cart" onClick={closeMenu}>
+                  🛒 ({cartCount})
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="hamburger" onClick={toggleMenu}>
+            ☰{cartCount > 0 && <span className="cart-count">{cartCount}</span>}
+          </div>
+        </>
+      ) : (
+        <div className="cart-only">
+          <Link to="/cart" className="onlyhomecart" onClick={closeMenu}>
+            🛒 Your Garden ({cartCount})
+          </Link>
+        </div>
+      )}
     </nav>
   );
 }
