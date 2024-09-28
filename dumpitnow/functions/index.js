4df79e373ff414035eb3538b3f0f1f3f12c2aec6
Suryapp8 <suryapp876@gmail.com>
2024-09-28 14:33:37 +0530

    bug fixed

--- dumpitnow/functions/index.js
+++ dumpitnow/functions/index.js
@@ -5,26 +5,26 @@ const twilio = require("twilio");
 // Initialize Firebase admin SDK
 admin.initializeApp();
 
-// Twilio credentials
-const accountSid = "your_twilio_account_sid";
-const authToken = "your_twilio_auth_token";
+// Get Twilio credentials from environment variables
+const accountSid = functions.config().twilio.sid;
+const authToken = functions.config().twilio.token;
 const client = twilio(accountSid, authToken);
 
 // Specify the phone number to send updates to
 const notificationPhoneNumber = "+917060975127"; // Your phone number
 
-// Firestore trigger for new orders
+
 exports.sendOrderNotification = functions.firestore
   .document("Order-Details/{orderId}")
   .onCreate((snapshot, context) => {
     const orderData = snapshot.data();
     const messageBody = `New Order Received from ${orderData.user}. Pickup Time: ${orderData.pickUpTime}`;
 
-    // Send SMS via Twilio
+    
     return client.messages
       .create({
         body: messageBody,
-        from: "your_twilio_phone_number", // Replace with your Twilio number
+        from: functions.config().twilio.phone, 
         to: notificationPhoneNumber,
       })
       .then((message) => {
